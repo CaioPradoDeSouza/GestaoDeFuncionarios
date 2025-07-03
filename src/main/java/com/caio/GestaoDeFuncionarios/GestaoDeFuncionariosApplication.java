180d5c0fc8dd374841e762bb5d9ff9047c53f8b0
cpsso <cpsso@192.168.0.253>
2025-07-03 15:55:35 -0300

    First commit: Criado projeto gestão de funcionários.

---
+++ src/main/java/com/caio/GestaoDeFuncionarios/GestaoDeFuncionariosApplication.java
@@ -0,0 +1,13 @@
+package com.caio.GestaoDeFuncionarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeFuncionariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeFuncionariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/GestaoDeFuncionarios/GestaoDeFuncionariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.GestaoDeFuncionarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeFuncionariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
